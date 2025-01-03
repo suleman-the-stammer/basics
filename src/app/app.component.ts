0ce94285f936541a806cf73d3a75f970df06f4db
stammer1 <stammer786@gmail.com>
2025-01-03 22:28:15 +0500

    Basics Concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` Welcome Sir {{title}} 
-  <app-header>
-  `,
+  imports: [ RouterOutlet , HeaderComponent , FooterComponent],
+ 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'basics';
+  title = 'Stammer';
 }
